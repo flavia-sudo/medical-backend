df727aa74aa7be8155faf34cec5ac621570bed69
flaviasudo <kihahuflavia@gmail.com>
2025-07-08 10:29:40 +0300

    time

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -42,7 +42,7 @@ export const AppointmentTable = pgTable("appointment", {
     userId: integer("userId").references(() => UserTable.userId).notNull(),
     doctorId: integer("doctorId").references(() => DoctorTable.doctorId).notNull(),
     appointmentDate: date("Date").notNull(),
-    time: timestamp("Time"),
+    time: new Date("Time"),
     totalAmount: decimal("Total Amount", { precision: 10, scale: 2 }).notNull(),
     status: AppointmentEnum("Status").notNull().default("pending"),
     createdAt: timestamp("Created At", { withTimezone: true }).defaultNow(),
