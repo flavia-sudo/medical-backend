4e5bd325f31ff2fc2e09d6301003f5b8886d9ed3
flaviasudo <kihahuflavia@gmail.com>
2025-07-09 10:29:41 +0300

    authentication

--- src/doctor/doctor.router.ts
+++ src/doctor/doctor.router.ts
@@ -1,5 +1,6 @@
 import { Express, Response, Request, NextFunction } from 'express';
 import { createDoctorController, deleteDoctorController, getDoctorByIdController, getDoctorsController, updateDoctorByIdController } from './doctor.controller';
+import { isAdmin, isAuthenticated } from '../middleware/auth.middleware';
 
 const doctor = (app: Express) => {
     //create doctor
@@ -15,6 +16,8 @@ const doctor = (app: Express) => {
 
     //get all doctors
     app.route('/doctor_all').get(
+        isAuthenticated,
+        isAdmin,
         async (req:Request, res:Response, next:NextFunction) => {
             try {
                 await getDoctorsController(req,res)
