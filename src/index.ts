a413ab3681a7b40f6656f86bf26b7a2773189bf6
flaviasudo <kihahuflavia@gmail.com>
2025-07-16 14:58:53 +0300

    Added cors for authorization in frontend

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,14 @@ import complaint from "./complaints/complaint.router";
 import transaction from "./transaction/transaction.router";
 import prescription from "./prescription/prescription.router";
 import auth from "./auth/auth.router";
+import cors from "cors";
 const initializeApp = () => {
 const app = express();
 //middleware to parse JSON bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 auth(app);
 user(app);
